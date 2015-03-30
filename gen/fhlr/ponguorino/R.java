80c94e5aaf2fb8d6e27083431bf2227cac67ff19
Julien Fauvel <fj130127@localhost.localdomain>
2015-03-30 10:04:10 +0200

    menu

--- gen/fhlr/ponguorino/R.java
+++ gen/fhlr/ponguorino/R.java
@@ -25,7 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int jouer=0x7f080000;
+        public static final int quitter=0x7f080002;
+        public static final int scores=0x7f080001;
     }
     public static final class layout {
         public static final int activity_accueil=0x7f030000;
@@ -36,7 +39,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int jouer=0x7f050003;
+        public static final int pong=0x7f050001;
+        public static final int quitter=0x7f050005;
+        public static final int scores=0x7f050004;
     }
     public static final class style {
         /** 
